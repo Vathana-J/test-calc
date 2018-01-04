44d44bd205b13c5df69b11e0ee63b46a608dc014
Vathana-J <vathana.jeyasooriya@gmail.com>
2018-01-03 16:55:21 -0800

    added code to call tables class

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-from master import master_cls
-myObject = master_cls()
+
+from master import basic_maths
+from mat_tables import tables
+
+myObject = basic_maths()
 print ("Enter the first integer:")
 a = int(raw_input())
 print ("Enter the second integer:")
 b = int(raw_input())
 print ("Sum of the 2 integers is : %d" % (myObject.addTwoNumbers(a,b)))
+print ("Multiplication of the 2 integers is : %d" % (myObject.multiplyTwoNumbers(a,b)))
+
+print ("Enter the number you want the multiplcation tables for:")
+mul_tab = int(raw_input())
+tablesObj = tables()
+tablesObj.generate_table(mul_tab)
