9a05990e8fa124ab711584bccfd52be8ee5ed5ef
Vathana-J <vathana.jeyasooriya@gmail.com>
2018-01-03 16:53:19 -0800

    some changes to master

--- master.py
+++ master.py
@@ -1,7 +1,12 @@
-class master_cls:
+class basic_maths:
     @staticmethod
     def addTwoNumbers(a, b):
         c= a + b
         return c
+
+    @staticmethod
+    def multiplyTwoNumbers(a,b):
+        return a*b
+
 
 
